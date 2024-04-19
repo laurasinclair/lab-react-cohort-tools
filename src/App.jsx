216a3da5884c98925ca2e84c351fe7f195a7152a
Laura Sinclair <l.sinclair@tuta.io>
2024-04-19 14:46:54 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import StudentDetailsPage from "./pages/StudentDetailsPage";
-import UserProfilePage from "./pages/UserProfilePage";
-
+import './App.css'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import StudentDetailsPage from './pages/StudentDetailsPage'
+import UserProfilePage from './pages/UserProfilePage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
+	return (
+		<div className="App relative z-20 pt-20">
+			<Navbar />
 
-  return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
-
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
-    </div>
-  );
+			<div className="pages">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route 
+            path={`/students/:studentId`}
+            element={<StudentDetailsPage />} 
+          />
+					<Route path="/profile" element={<UserProfilePage />} />
+				</Routes>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
